700a2d19ba432cdd230425e891d364d99767d4bc
Dapinder-Kaur <gilldapinder515@gmail.com>
2025-07-24 15:35:22 -0500

    Add print board

--- src/main.py
+++ src/main.py
@@ -7,3 +7,5 @@ if __name__ == "__main__":
         print(f"hello world {sys.argv[1]}")
     else:
         print("Good Bye")
+        
+    
--- src/player.py
+++ src/player.py
@@ -1,3 +1,27 @@
 import math
 import random
 
+class Player:
+    def __init__(self, letter):
+        self.letter = letter
+        
+    def get_move(self, game):
+        pass
+    
+    
+class RandomComputerPLayer(Player):
+    def __init__(self, letter):
+        super().__init_(letter)
+        
+    def get_move(self, game):
+        pass
+    
+class HumanPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+        
+    def get_move(self, game):
+        pass
+    
+    
+    
