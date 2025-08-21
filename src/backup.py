4ddaa318894227d4f6db57c8a5a0c4204e767e35
Dapinder-Kaur <gilldapinder515@gmail.com>
2025-08-20 20:59:04 -0500

    Add backup script

---
+++ src/backup.py
@@ -0,0 +1,23 @@
+import os
+import shutil
+import datetime
+import schedule
+import time
+
+source_dir = r"C:\Users\gilld\OneDrive\Documents\Projects\python-project\folder"
+destination_dir = r"C:\Users\gilld\OneDrive\Documents\Projects\python-project\backups"
+
+def copy_folder_to_directory(source, dest):
+    today = datetime.date.today()
+    dest_dir = os.path.join(dest, str(today))
+
+    try:
+        shutil.copytree(source, dest_dir)
+        print(f"Folder copied to: {dest_dir}")
+
+    except FileExistsError:
+        print(f"Folder already exists: {dest_dir}")
+
+    
+copy_folder_to_directory(source_dir, destination_dir)
+
